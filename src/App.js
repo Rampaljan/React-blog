2f1a7ce8f2de3fb1ceb0c05489c63d2b98b5be4c
Rampaljan <rampaljan2008@gmail.com>
2025-04-30 14:30:10 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -11,9 +11,37 @@ import { Route, Routes, Link, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 const App = () => {
+  const [search, setSearch] = useState('');
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
+
 return(
   <div className="App">
-    <Header />
+    <Header  title="React blog" />
     <Nav />
     <Routes>
       <Route exact path="/" element={<Home/>} />
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,23 @@
-const Nav = () => {
+import { Link } from "react-router-dom"
+
+const Nav = ({search, setSearch}) => {
     return (
-        <nav >
-            <h1>Nav</h1>
+        <nav className="Nav">
+            <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
+                <label htmlFor="search">Search posts</label>
+                <input
+                    type="text"
+                    id="search"
+                    placeholder="Search posts"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </form>
+            <ul className="navList">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/post">Post</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
         </nav>
     )
 }
